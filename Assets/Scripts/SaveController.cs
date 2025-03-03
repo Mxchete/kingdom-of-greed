47c92ed28e5eb7dae275838dcdc101a349608bc0
Santy <SantiagoEcheverry25@gmail.com>
2025-03-03 10:20:05 -0500

    Inventory done, Version 1 of Hotbar, Start of town

--- Assets/Scripts/SaveController.cs
+++ Assets/Scripts/SaveController.cs
@@ -8,11 +8,14 @@ public class SaveController : MonoBehaviour
 {
     private string saveLocation;
     private InventoryController inventoryController;
+    private HotbarController hotbarController;
     // Start is called before the first frame update
     void Start()
     {
         saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
         inventoryController = FindObjectOfType<InventoryController>();
+        hotbarController = FindObjectOfType<HotbarController>();
+
         LoadGame();
     }
 
@@ -23,7 +26,8 @@ public class SaveController : MonoBehaviour
         {
             playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position,
             mapBoundary = FindObjectOfType<CinemachineConfiner>().m_BoundingShape2D.gameObject.name,
-            inventorySaveData = inventoryController.GetInventoryItems()
+            inventorySaveData = inventoryController.GetInventoryItems(),
+            hotbarSaveData = hotbarController.GetHotbarItems()
         };//this is writing a json
 
         File.WriteAllText(saveLocation, JsonUtility.ToJson(saveData));
@@ -36,6 +40,7 @@ public class SaveController : MonoBehaviour
             GameObject.FindGameObjectWithTag("Player").transform.position = saveData.playerPosition;
             FindObjectOfType<CinemachineConfiner>().m_BoundingShape2D = GameObject.Find(saveData.mapBoundary).GetComponent<PolygonCollider2D>();
             inventoryController.SetInventoryItems(saveData.inventorySaveData);
+            hotbarController.SetHotbarItems(saveData.hotbarSaveData);
         }
         else
         {
--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -8,6 +8,7 @@ public class SaveData
     // Start is called before the first frame update
     public Vector3 playerPosition;
     public string mapBoundary;//boundry name either maze or otherBoundry so far
-    // Update is called once per frame
+    
     public  List<InventorySaveData> inventorySaveData;
+    public List<InventorySaveData> hotbarSaveData;
 }
